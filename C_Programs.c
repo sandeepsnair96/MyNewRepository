b4a3306b2f936b58cec8f7a94b2c393318656748
sandeepsnair96 <sandeepsnair96@gmail.com>
2020-09-22 13:20:50 +0530

    Updating First Program

---
+++ C_Programs.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    char a = 'A';
+    int x;
+    x = 125;
+    float p = 10.25, q = 18.76;
+    printf("%c is stored at address %u. \n", a, &a);
+    printf("%d is stored at address %u. \n", x, &x);
+    printf("%f is stored at address %u. \n", p, &p);
+    printf("%f is stored at address %u. \n", q, &q);
+    return (0);
+}
